90ccd35f06e1d9cd7eeeebcf59c0d06719a90576
Bilbia <biamcs2000@gmail.com>
2021-03-30 15:59:41 -0300

    testes de unidade done

--- dna.c
+++ dna.c
@@ -18,12 +18,42 @@ void load_string(FILE *file, int *p, char *s, int t) {
 
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-    return 0;
+    if(n==0 || m==0){
+        length[n][m] = 0;
+        return length[n][m];
+    }
+
+    //se o length ja tiver sido calculado
+    if(length[n][m]!=-1){
+        return length[n][m];
+    }
+
+    //se o length n tiver sido calculado ainda
+    if(a[n-1] == b[m-1]){
+        length[n][m] = mlcs_w(a,n-1,b,m-1,length) + 1;
+        return length[n][m];
+    }
+
+    //calcula a subsequencia da esquerda e da direita da arvore
+    int esq = mlcs_w(a,n-1,b,m,length);
+    int dir = mlcs_w(a,n,b,m-1,length);
+    int max = esq;
+    if(esq<dir){
+        max = dir;
+    }
+    length[n][m] = max;
+    return length[n][m];
 }
 
 
 int mlcs(char a[], int n, char b[], int m) {
-    return 1;
+    int length[MAX_SIZE + 1][MAX_SIZE + 1];
+    for (int i=0; i<=n; i++){
+        for (int j=0; j<=m; j++){
+            length[i][j] = -1;
+        }
+    }
+    return mlcs_w(a,n,b,m,length);
 }
 
 
